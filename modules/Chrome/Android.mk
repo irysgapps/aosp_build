0160b791e65f0a863ddb7169f56ea61c4b7fbbae
Irys GApps <irysgapps@gmail.com>
2017-05-16 14:02:39 -0400

    packages: medium

--- modules/Chrome/Android.mk
+++ modules/Chrome/Android.mk
@@ -4,7 +4,7 @@ include $(GAPPS_CLEAR_VARS)
 LOCAL_MODULE := Chrome
 LOCAL_PACKAGE_NAME := com.android.chrome
 
-GAPPS_LOCAL_OVERRIDES_MIN_VARIANT := stock
+GAPPS_LOCAL_OVERRIDES_MIN_VARIANT := medium
 GAPPS_LOCAL_OVERRIDES_PACKAGES := Browser Browser2 BrowserProviderProxy Chromium Fluxion Gello
 
 include $(BUILD_GAPPS_PREBUILT_APK)
